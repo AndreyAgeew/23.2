78f04c192726daa4f6b40180e97b3f827593c8e7
Andrey Ageew <faceofdeviler@gmail.com>
2023-08-28 16:27:23 +0300

    19.2 exc

---
+++ catalog/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+
+def home(request):
+    if request.method == 'GET':
+        return render(request, "catalog/home.html")
+
+
+def contacts(request):
+    if request.method == 'POST':
+        visiter = dict()
+        visiter['name'] = request.POST.get('name', None)
+        visiter['phone'] = request.POST.get('phone', None)
+        visiter['message'] = request.POST.get('message', None)
+        print(visiter)
+    return render(request, "catalog/contacts.html")
