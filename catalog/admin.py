ee9e2a64493fc693a4a04222ac7e112f966d9446
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-04 00:16:55 +0300

    добавление админ панели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Product, Category, Contact
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = "pk", "name", "price", "category"
+    list_display_links = "pk", "name"
+    list_filter = ('category',)
+    search_fields = "name", "description"
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = "pk", "name"
+
+
+@admin.register(Contact)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = "pk", "country", "inn", "address"
