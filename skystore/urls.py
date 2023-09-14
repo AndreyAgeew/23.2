fbf2d8ffb8e3cc8a2e1c314e49ee9600f4e0c04f
Andrey Ageew <faceofdeviler@gmail.com>
2023-09-14 18:06:45 +0300

    update urls

--- skystore/urls.py
+++ skystore/urls.py
@@ -19,14 +19,11 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-from catalog.views import ProductDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls')),
-    path('product/<pk>', ProductDetailView.as_view()),
-    path('blog/', include('blog.urls'))
-
+    path('blog/', include('blog.urls')),
 ]
 
 if settings.DEBUG:
